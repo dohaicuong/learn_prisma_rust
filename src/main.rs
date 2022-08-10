2a668715499e9f507f6f5e3010d532489d80b410
Eric Do <beatyshot@gmail.com>
2022-08-10 23:37:35 +1000

    create and query posts

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,30 @@ use dotenv;
 
 mod prisma;
 
+use prisma::post;
+
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
 
     let client = prisma::new_client().await.unwrap();
 
-    let posts = client.post().find_many(vec![]).exec().await.unwrap();
+    let post = client
+        .post()
+        .create(
+            post::title::set("First post".to_string()),
+            vec![],
+        )
+        .exec()
+        .await
+        .unwrap();
+    println!("{:?}", post);
 
+    let posts = client
+        .post()
+        .find_many(vec![])
+        .exec()
+        .await
+        .unwrap();
     println!("{:?}", posts);
-
-    println!("Hello, world!");
 }
